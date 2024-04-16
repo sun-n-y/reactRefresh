958017baf4352c6f1ff46884cd695a3dcf83e6cc
sunny patel <sunnykpatel.skp@gmail.com>
2024-04-16 17:46:40 -0400

    add lorem ipsum form

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,40 @@
+import { useState } from 'react';
+import text from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(0);
+  const [loremIpsum, setLoremIpsum] = useState([]);
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoremIpsum(text.slice(0, parseInt(count)));
+  };
+
+  return (
+    <form>
+      <label htmlFor="number">paragraph</label>
+      <input
+        type="number"
+        id="number"
+        min={1}
+        max={8}
+        step={1}
+        value={count}
+        onChange={handleChange}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        generate
+      </button>
+      <div className="text-container">
+        {loremIpsum.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </div>
+    </form>
+  );
 };
 export default App;
